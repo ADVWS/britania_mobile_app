1352e73ef8a6e0da27c290742c733fb80460f623
NapapolKwanthong <napapol@advws.com>
2021-12-22 18:05:55 +0700

    add page and moudle font style

---
+++ britania/Shared/britaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  britaniaApp.swift
+//  Shared
+//
+//  Created by Napapol Kwanthong on 22/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct britaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
