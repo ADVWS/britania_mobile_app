db770c086434fbd7da8fd6c2bcfaa36058f28edb
Sunnyshineshow <sun.napon11@gmail.com>
2022-03-21 15:01:19 +0700

    Notification Number

--- src/component/Account_component/AccountHeader.js
+++ src/component/Account_component/AccountHeader.js
@@ -7,41 +7,71 @@ import { useRecoilState } from "recoil";
 import * as Global from "../../globalState";
 
 import { Styles } from "../../styles";
-import Store from '../../store';
-import Script from '../../script/Notification_script';
-import Key from '../../KEYS.json'
+import Store from "../../store";
+import Script from "../../script/Notification_script";
+import Key from "../../KEYS.json";
 
-const AccountHeader = ({ }) => {
+const AccountHeader = ({}) => {
   var screen = "Account";
   const [userType, setUserType] = useRecoilState(Global.userType);
   const [LANG, setLANG] = useRecoilState(Global.Language);
-  const [counter, setCounter] = React.useState('-')
+  const [counter, setCounter] = React.useState("-");
 
   const setCountNotify = () => {
     Store.getLocalStorege(Key.TOKEN, (tk) => {
-      const token = tk.detail.token
+      const token = tk.detail.token;
       Script.notificationCountUnread(token, (res) => {
-        setCounter(res)
-      })
-    })
-  }
+        setCounter(res);
+      });
+    });
+  };
+
+  setCountNotify();
 
   function setNotify() {
     if (userType !== 1) {
       return (
-        <TouchableOpacity disabled={true} onPress={() => navigate.navigate("Notify", { screen })}>
-          <MaterialIcons name="notifications-none" size={30} color={'#000'} />
-        </TouchableOpacity>)
+        <TouchableOpacity
+          disabled={true}
+          onPress={() => navigate.navigate("Notify", { screen })}
+        >
+          <MaterialIcons name="notifications-none" size={30} color={"#000"} />
+        </TouchableOpacity>
+      );
     } else {
       return (
-        <TouchableOpacity onPress={() => navigate.navigate("Notify", { screen })}>
-          <MaterialIcons name="notifications-none" size={30} color={'#555555'} />
-          {counter !== '-' && counter !== 0 &&
-            <View style={{ backgroundColor: "red", borderRadius: 100, height: 25, width: 25, position: 'absolute', top: -7, left: 15, alignItems: 'center', justifyContent: 'center' }}>
-              <Text style={{ color: '#FFF', fontSize: 12 }}>{setCountNotify()}{counter}</Text>
+        <TouchableOpacity
+          onPress={() => navigate.navigate("Notify", { screen })}
+        >
+          <MaterialIcons
+            name="notifications-none"
+            size={30}
+            color={"#555555"}
+          />
+          {counter !== "-" && counter !== 0 && (
+            <View
+              style={[
+                Styles.mainColor,
+                {
+                  borderRadius: 100,
+                  height: 20,
+                  width: 20,
+                  position: "absolute",
+                  top: -7,
+                  left: 15,
+                  alignItems: "center",
+                  justifyContent: "center",
+                },
+              ]}
+            >
+              <Text style={{ color: "#FFF", fontSize: 12 }}>
+                {setCountNotify()}
+                {counter}
+              </Text>
             </View>
-          }
-        </TouchableOpacity>)
+          )}
+        </TouchableOpacity>
+      );
     }
   }
 
